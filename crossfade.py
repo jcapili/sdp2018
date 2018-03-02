31148ae24e522d2b27f0f9cbc99ec18602904a73
Mark Hattori <mark.hattori@yahoo.com>
2018-03-02 14:23:05 -0800

    generates alpha wave which fades into a theta wave

--- crossfade.py
+++ crossfade.py
@@ -10,18 +10,18 @@ tone2 = Sine(210).to_audio_segment(duration=2000)
 left1 = tone1
 right1 = tone2
 
-tone3 = Sine(100).to_audio_segment(duration=2000)
-tone4 = Sine(105).to_audio_segment(duration=2000)
+tone3 = Sine(200).to_audio_segment(duration=2000)
+tone4 = Sine(204).to_audio_segment(duration=2000)
 
 left2 = tone3
 right2 = tone4
 
 alpha = AudioSegment.from_mono_audiosegments(left1, right1)
-stereo2 = AudioSegment.from_mono_audiosegments(left2, right2)
+theta = AudioSegment.from_mono_audiosegments(left2, right2)
 
 #descend = alpha.append(stereo2, crossfade=1000)
-
-play(alpha)
+alpha_to_theta = alpha.append(theta, crossfade=1000)
+play(alpha_to_theta)
 
 #play(descend)
 
