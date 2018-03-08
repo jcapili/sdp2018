f7a03c6f299dabcc65b950726c437ae50c28e604
Mark Hattori <mark.hattori@yahoo.com>
2018-03-07 23:28:59 -0800

    changed length of tone, changed from alpha to theta wave

--- crossfade.py
+++ crossfade.py
@@ -4,14 +4,14 @@ from pydub import AudioSegment
 from pydub.generators import Sine
 from pydub.playback import play
 
-tone1 = Sine(200).to_audio_segment(duration=2000)
-tone2 = Sine(210).to_audio_segment(duration=2000)
+tone1 = Sine(200).to_audio_segment(duration=3000)
+tone2 = Sine(210).to_audio_segment(duration=3000)
 
 left1 = tone1
 right1 = tone2
 
-tone3 = Sine(200).to_audio_segment(duration=2000)
-tone4 = Sine(204).to_audio_segment(duration=2000)
+tone3 = Sine(200).to_audio_segment(duration=3000)
+tone4 = Sine(204).to_audio_segment(duration=3000)
 
 left2 = tone3
 right2 = tone4
