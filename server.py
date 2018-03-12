249c5a50088acaa2c1a21a510975f13acfdcac09
Jason Capili <jcapili@scu.edu>
2018-03-11 20:00:36 -0700

    Continuous Sounds
    Everything works with fading in and out except for restart session, haven't integrated with server yet

--- server.py
+++ server.py
@@ -1,21 +1,5 @@
-# http://developer.choosemuse.com/research-tools-example/grabbing-data-from-museio-a-few-simple-examples-of-muse-osc-servers#python
-#1. Gamma State: (30 — 100Hz) This is the state of hyperactivity and active learning. Gamma state is the most opportune time to retain information. This is why educators often have audiences jumping up and down or dancing around — to increase the likelihood of permanent assimilation of information. If over stimulated, it can lead to anxiety.
-#
-#2. Beta State: (13 — 30Hz) Where we function for most of the day, Beta State is associated with the alert mind state of the prefrontal cortex. This is a state of the “working” or "thinking mind": analytical, planning, assessing and categorizing.
-#
-#3. Alpha State: (9 — 13Hz) Brain waves start to slow down out of thinking mind. We feel more calm, peaceful and grounded. We often find ourselves in an “alpha state” after a yoga class, a walk in the woods, a pleasurable sexual encounter or during any activity that helps relax the body and mind. We are lucid, reflective, have a slightly diffused awareness. The hemispheres of the brain are more balanced (neural integration).
-#
-#4. Theta State: (4 — 8Hz) We're able to begin meditation. This is the point where the verbal/thinking mind transitions to the meditative/visual mind. We begin to move from the planning mind to a deeper state of awareness (often felt as drowsy), with stronger intuition, more capacity for wholeness and complicated problem solving. The Theta state is associated with visualization.
-#
-#5. Delta State: (1—3 Hz) Tibetan monks who have been meditating for decades can reach this in an alert, wakened phase, but most of us reach this final state during deep, dreamless sleep.
-from pydub import AudioSegment
-from pydub.generators import Sine
-from pydub.playback import play
-
-
 import argparse
 import math
-#import pyaudio
 import numpy as np
 import sys
 
@@ -23,7 +7,6 @@ from tkinter import *
 from tkinter.ttk import *
 import os
 import threading
-#import multiprocessing
 import time
 
 from pythonosc import dispatcher
@@ -92,7 +75,6 @@ def end_server():
     isServing = False
     print( isServing )
 
-
 #   This function recursively calls itself so that we don't need to use .serve_forever()
 def run_server():
     # isServing allows us to start/stop the stream of data without having to rerun the program
@@ -143,15 +125,14 @@ if __name__ == "__main__":
     bottom = Frame(window)
     bottom.pack(side=BOTTOM)
     
-    # use lambdas so the command functions don't run on button initialization
-    Button(top, text="Start Session", command=lambda : sound_generation.start_all_sounds()).pack()
-    Button(bottom, text="Stop Session", command=lambda : end_server()).pack()
-    Button(bottom, text="Restart Session", command=lambda : begin_server()).pack()
+    Button(top, text="Start Session", command= sound_generation.start_all_sounds).pack()
+    Button(bottom, text="Stop Session", command=sound_generation.stop_all_sounds).pack()
+    Button(bottom, text="Restart Session", command=sound_generation.restart_all_sounds).pack()
 
     
     # start the thread
-    threading.Thread(target=run_server, daemon=True).start()
-    
+    threading.Thread(name="server", target=run_server, daemon=True).start()
+
     # start the GUI
     window.mainloop()
 
--- sound_generation.py
+++ sound_generation.py
@@ -3,11 +3,22 @@ from pydub.generators import Sine
 from pydub.playback import play
 
 import threading
+import time
 
 tone_length = 2000
+fade_length = int(tone_length * 0.25)
 
+# Sleep time between sounds in seconds, used with the time library
+sleep_time = tone_length * 0.95 / 1000
+
+# If this is True, the current binaural beat is the first one, so play the second one.
+# If this is False, the current binaural beat is the second one, so play the first one.
+thread_tracker = True
+
+isPlaying = True
 
 def play_tone_1():
+    global tone_length, fade_length
     tone1 = Sine(200).to_audio_segment(duration=tone_length)
     tone2 = Sine(210).to_audio_segment(duration=tone_length)
     
@@ -20,8 +31,8 @@ def play_tone_1():
     left2 = tone3
     right2 = tone4
     
-    alpha = AudioSegment.from_mono_audiosegments(left1, right1)
-    theta = AudioSegment.from_mono_audiosegments(left2, right2)
+    alpha = AudioSegment.from_mono_audiosegments(left1, right1).fade_in(duration=fade_length)
+    theta = AudioSegment.from_mono_audiosegments(left2, right2).fade_out(duration=fade_length)
     
     #descend = alpha.append(stereo2, crossfade=1000)
     alpha_to_theta = alpha.append(theta, crossfade=1000)
@@ -40,8 +51,8 @@ def play_tone_2():
     left2 = tone3
     right2 = tone4
     
-    alpha = AudioSegment.from_mono_audiosegments(left1, right1)
-    theta = AudioSegment.from_mono_audiosegments(left2, right2)
+    alpha = AudioSegment.from_mono_audiosegments(left1, right1).fade_in(duration=fade_length)
+    theta = AudioSegment.from_mono_audiosegments(left2, right2).fade_out(duration=fade_length)
     
     #descend = alpha.append(stereo2, crossfade=1000)
     alpha_to_theta = alpha.append(theta, crossfade=1000)
@@ -51,16 +62,60 @@ def sound_effects():
     sound1 = AudioSegment.from_file("/Users/jasoncapili/Documents/GitHub/sdp2018/birds.m4a", format="m4a")
     play(sound1)
 
+def timer():
+    start = time.time()
+    while(1):
+        # sleep_time - 1 accounds for sleep before starting timer
+        if (time.time() - start) > (sleep_time - 1):
+            global thread_tracker
+            if( thread_tracker ):
+                print("starting bin beat 2")
+                binaural_thread_2()
+                thread_tracker = False
+            else:
+                print("starting bin beat 1")
+                binaural_thread_1()
+                thread_tracker = True
+            break
 
-def start_binaural_thread_1():
+def start_timer():
+    global isPlaying
+    if( isPlaying ):
+        threading.Thread(target=timer, daemon=True).start()
+
+def binaural_thread_1():
+    global sleep_time, isPlaying
     threading.Thread(target=play_tone_1, daemon=True).start()
+    # Sleep to avoid seg fault
+#    time.sleep(sleep_time)
+#    if isPlaying:
+#        binaural_thread_2()
+    time.sleep(1)
+    start_timer()
 
-def start_binaural_thread_2():
+def binaural_thread_2():
+    global sleep_time, isPlaying
     threading.Thread(target=play_tone_2, daemon=True).start()
+    # Sleep to avoid seg fault
+#    time.sleep(sleep_time)
+#    if isPlaying:
+#        binaural_thread_1()
+    time.sleep(1)
+    start_timer()
 
 def start_sound_effects_thread():
     threading.Thread(target=sound_effects, daemon=True).start()
 
+def stop_all_sounds():
+    global isPlaying
+    isPlaying = False
+
+def restart_all_sounds():
+    global isPlaying
+    isPlaying = True
+
 def start_all_sounds():
-    threading.Thread(target=play_tone_1, daemon=True).start()
-    threading.Thread(target=play_tone_2, daemon=True).start()
+    binaural_thread_1()
+    start_sound_effects_thread()
+
+
