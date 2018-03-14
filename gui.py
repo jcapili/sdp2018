10b315d6ca5c3fe647b85d8c1ffc22e3741fba11
mailoh <maile.naito@gmail.com>
2018-03-13 20:43:27 -0700

    nothing

--- gui.py
+++ gui.py
@@ -1,37 +1,23 @@
 from tkinter import *
+from tkinter.ttk import *
 import os
 import threading
 
-# Create Tk window
-window = Tk()
+import sound_generation
+def test():
+    sound_generation.switchTones = True
 
-# Add text to the window via a label
-label = Label(window, text="Hello World")
-label.pack() # Make the label visible
-
-top = Frame(window)
-top.pack()
-bottom = Frame(window)
-bottom.pack(side=BOTTOM)
-
-#   Separate imports for server because mutual imports are bad according to the Internet
-def start_eeg():
-    import server
-    server.begin_server()
-
-#   Separate imports for server because mutual imports are bad according to the Internet
-def stop_eeg():
-    import server
-    server.end_server()
-
-
-# Put the window in a loop so that it doesn't just open, print the label, and then close
-def main():
-    # use lambdas so the command functions don't run on button initialization
-    button1 = Button(top, text="Start EEG data", fg="red", command=lambda : start_eeg())
-    button2 = Button(bottom, text="Stop EEG data", fg="green", command=lambda : stop_eeg())
-
-    button1.pack()
-    button2.pack()
+def start_gui():
+    window = Tk() # Create Tk window
+    Label(window, text="Binaural Beats with Muse").pack()
+    
+    top = Frame(window)
+    top.pack()
+    bottom = Frame(window)
+    bottom.pack(side=BOTTOM)
+    
+    Button(top, text="Start Session", command = sound_generation.start_all_sounds).pack()
+    Button(bottom, text="Stop Session", command= sound_generation.stop_all_sounds).pack()
+    Button(bottom, text="Switch Tones", command=test).pack()
     
     window.mainloop()
